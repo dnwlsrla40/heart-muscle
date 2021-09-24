5f1622b1349f24faf550ac8cf884e80b605676ed
yooonnn <>
2021-09-24 17:58:30 +0900

    득근일지 DB 저장, 글 목록(이름/제목) 보여주기

--- app.py
+++ app.py
@@ -13,5 +13,25 @@ db = client.dbMuscle
 def homework():
     return render_template('index.html')
 
+@app.route('/diary', methods=['POST'])
+def write_diary():
+    name_receive = request.form['name_give']
+    title_receive = request.form['title_give']
+    diary_receive = request.form['diary_give']
+
+    doc = {
+        'name': name_receive,
+        'title': title_receive,
+        'diary': diary_receive
+    }
+
+    db.dbMuscle.insert_one(doc)
+    return jsonify({'msg': '저장 완료!'})
+
+@app.route('/diary', methods=['GET'])
+def read_diary():
+    diaries = list(db.dbMuscle.find({},{'_id': False}))
+    return jsonify({'diary_list': diaries})
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
