e2be44e7417212c4aeb1638c386c06a5b350c79a
김우진 <dnwlsrla40@naver.com>
2021-09-25 23:31:30 +0900

    [추가] 영상페이지 youtube data api로 검색 데이터 받기까지 성공

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# app.py
+import requests
 from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
@@ -7,11 +9,40 @@ from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 db = client.dbMuscle
 
+@app.route('/movie')
+def movie():
+    return render_template('movie.html')
 
 ## HTML 화면 보여주기
 @app.route('/')
 def homework():
     return render_template('index.html')
+
+@app.route('/movies', methods=['GET'])
+def test():
+    # q = "홈트 "
+    q = request.args.get('q')
+    print(q)
+
+    optionParams = {
+        "q": q,
+        "part": "snippet",
+        "key": "AIzaSyARx6jH12f_mg-uAm_1bmlqlR8Ov69bKYY",
+        "maxResults": "5",
+        "type": "video",
+        "videoDuration": "medium",  # 영상 길이 : 4분이상, 20분 이하
+    }
+    request_url = "https://www.googleapis.com/youtube/v3/search?"
+
+    for option in optionParams:
+        request_url += option + "=" + optionParams[option] + "&"
+
+    data = requests.get(request_url)
+    jsonized_data = data.json()
+    comments_list = {
+        "items": jsonized_data["items"]
+    }
+    return jsonify(comments_list)
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
