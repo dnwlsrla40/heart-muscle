c1ef169d1e2ead5e63512278af7fbd1dd4e1c9cc
yooonnn <90806892+yooonnn@users.noreply.github.com>
2021-09-27 17:28:27 +0900

    Update app.py
    [추가] 날짜, 조회수, 글번호 생성 및 DB 저장

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ app = Flask(__name__)
 
 from pymongo import MongoClient
 
-client = MongoClient('localhost', 27017)
+client = MongoClient("mongodb://localhost:27017/")
 db = client.dbMuscle
 
+import time
 
 ## HTML 화면 보여주기
 @app.route('/')
@@ -15,14 +16,17 @@ def homework():
 
 @app.route('/diary', methods=['POST'])
 def write_diary():
-    name_receive = request.form['name_give']
+    writer_receive = request.form['writer_give']
     title_receive = request.form['title_give']
-    diary_receive = request.form['diary_give']
+    content_receive = request.form['content_give']
 
     doc = {
-        'name': name_receive,
+        'writer': writer_receive,
         'title': title_receive,
-        'diary': diary_receive
+        'content': content_receive,
+        'created_at': time.strftime('%Y-%m-%d', time.localtime(time.time())),
+        'views': 0,
+        'count': db.dbMuscle.count()+1
     }
 
     db.dbMuscle.insert_one(doc)
@@ -32,6 +36,15 @@ def write_diary():
 def read_diary():
     diaries = list(db.dbMuscle.find({},{'_id': False}))
     return jsonify({'diary_list': diaries})
+
+@app.route('/diary/view', methods=['POST'])
+def update_view():
+    views_receive = request.form['view_give']
+    target_writer = db.dbMuscle.find_one({'writer': views_receive})
+    current_views = target_writer['views']
+    new_views = current_views + 1
+    db.dbMuscle.update_one({'writer': views_receive}, {'$set': {'views': new_views}})
+    return jsonify({'msg': '상세페이지로 이동합니다!'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
