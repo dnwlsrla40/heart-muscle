2965a3945a692b6a4e1960e44c2665451bf352e7
yooonnn <90806892+yooonnn@users.noreply.github.com>
2021-09-29 09:53:07 +0900

    Add files via upload
    [추가] 제목 클릭시 다음 페이지 연결

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import requests
 from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
@@ -14,10 +13,6 @@ import time
 @app.route('/')
 def homework():
     return render_template('index.html')
-  
-@app.route('/movie')
-def movie():
-    return render_template('movie.html')
 
 @app.route('/diary', methods=['POST'])
 def write_diary():
@@ -54,32 +49,6 @@ def update_view():
     new_views = current_views + 1
     db.dbMuscle.update_one({'writer': views_receive}, {'$set': {'views': new_views}})
     return jsonify({'msg': '상세페이지로 이동합니다!'})
-  
-@app.route('/movies', methods=['GET'])
-def test():
-    # q = "홈트 "
-    q = request.args.get('q')
-    print(q)
 
-    optionParams = {
-        "q": q,
-        "part": "snippet",
-        "key": "AIzaSyARx6jH12f_mg-uAm_1bmlqlR8Ov69bKYY",
-        "maxResults": "5",
-        "type": "video",
-        "videoDuration": "medium",  # 영상 길이 : 4분이상, 20분 이하
-    }
-    request_url = "https://www.googleapis.com/youtube/v3/search?"
-
-    for option in optionParams:
-        request_url += option + "=" + optionParams[option] + "&"
-
-    data = requests.get(request_url)
-    jsonized_data = data.json()
-    comments_list = {
-        "items": jsonized_data["items"]
-    }
-    return jsonify(comments_list)
-  
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
